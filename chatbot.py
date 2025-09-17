613b6b60e5538c66555831315e38308aca3a806a
Bruce Wefler <bruce.wefler@gmail.com>
2025-09-16 20:03:48 -0600

    Placed appropriate code into function form_response

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,20 @@
 from openai import OpenAI
-
 client = OpenAI()
 
+
 user_input = input("\nAsk something...\n\n")
+
+#Function to form reponse
+def form_response(model, messages):
+    response = client.chat.completions.create(
+        model = model,
+        messages = messages
+    )
+    return response.choices[0].message.content
 
 model = "gpt-3.5-turbo"
 messages = [
     {"role": "system", "content": "You are an assistant that always answers in the form of a poem."},
     {"role": "user", "content": user_input}
 ]
-response = client.chat.completions.create(
-    model = model,
-    messages = messages
-)
-
-response_for_user = response.choices[0].message.content
-print(response_for_user)
+print(form_response(model, messages))
